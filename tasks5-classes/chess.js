03b2ed1c43f3a72bc61284d5d8a745a6efec70c6
Yan <example@g.com>
2020-10-17 19:14:18 +0200

    Обновленные шахматы - работает проверка хода пешки

--- tasks5-classes/chess.js
+++ tasks5-classes/chess.js
@@ -1,65 +1,69 @@
 class Piece {
-	constructor(){
-		this.isConvertible = function(type) {
-		return (type == 'pawn') ? true : false;
-		};
-	}
-
+	constructor(color){this.color = color;}
 }
-
+// пустой объект для хранения ключей существующих фигур
+const pieces = {
+};
 class Pawn extends Piece{
-	constructor (name, color) {
-		super();
-		this.type = 'pawn';
+	constructor (name, color, space) {
+		super(color);
 		this.name = name;
-		this.color = color;
+		this.space = space;
 		this.motions = [1, 0];
+		this.space = space;
+		pieces[name] = space;
 		this.getMotion = function () {
 			return this.motions;
 		};
-		this.convertible = this.isConvertible (this.type);
-
+		this.convertible = true;
 	}
 }
-// bishop, knight, castle, queen, king
-class Field {
-	constructor() {
-		this.state = [['bb1', 'kb1', 'cb1', 'qb',  'kgb', 'cb2', 'kb2', 'bb2'],
-					  ['pb1', 'pb2', 'pb3', 'pb4', 'pb5', 'pb6', 'pb7', 'pb8'],
-					  [0    , 0    , 0    , 0    , 0    , 0    , 0    , 0    ],
-					  [0    , 0    , 0    , 0    , 0    , 0    , 0    , 0    ],
-					  [0    , 0    , 0    , 0    , 0    , 0    , 0    , 0    ],
-					  [0    , 0    , 0    , 0    , 0    , 0    , 0    , 0    ],
-					  ['pw1', 'pw2', 'pw3', 'pw4', 'pw5', 'pw6', 'pw7', 'pw8'],
-					  ['bw1', 'kw1', 'cw1', 'qw',  'kwg', 'cw2', 'kw2', 'bw2']];
-	}
 
-}
 
-class Game extends Field{
+class Game{
 	constructor() {
-		super();
+
 		this.takedPieces = {};
 		this.whoIsNext = 'white';
 		this.countOfmoves = 0;
 		this.movesHistory = {};
-		this.canIgoToSquare = function (x, y, xto, yto){
-			if(this.state[xto-1][yto-1] !== 0){
-				console.log('Выбранная ячейка занята');
-			} else {
-				console.log('Выбранная ячейка не занята');
+		this.canIgoToSquare = function (to){
+			// есди нет такого ключа в объекте фигур
+			if(!(to in pieces)) {
+				let flag = false;
+				let cell = to.split('');
+				if(cell[0] > 'h' || cell[1] > 8) {
+					console.log('клетка вне поля');
+					return false;
+					} else if (true){
+
+				for (let key in pieces) {
+
+					if(pieces[key] === to) {
+						console.log('Клетка уже занята, сюда ходить нельзя');
+						return false;
+					}
+
+				}
+
 				return true;
 			}
-			return false;
+			} else {
+				return false;
+			}
 
 		};
-		this.moveTo = function (color, piece, x, y, xto, yto) {
-			if(this.canIgoToSquare(x, y, xto, yto) || this.whoIsNext === color){
+		this.moveTo = function (piece, to) {
+			//если можно сходить на клетку to и ход фигур текущего цвета
+			if(this.canIgoToSquare(to) && this.whoIsNext === piece.color){
+				//меняем текущий цвет для следующего хода
 				this.whoIsNext = (this.whoIsNext === 'white')?'black':'white';
-
-				console.log('Yes ser!', color, piece, x, y, xto, yto);
+				piece.space = to; // двигаем фигуру на клетку to
+				console.log('Yes ser!', piece.space);
+				pieces[piece.name] = to; //меняем координаты фигуры в списке фигур
 			} else {
-				//Позволяет выбрать фигуру только в том случае, если цвет фигуры соответствует цвету игрока, который ходит
+				// если цвет фигуры не соответствует цвету игрока, который ходит
+				// или выбранная для хода клетка занята или вне поля
 				console.log('You have no move');
 			}
 		}
@@ -69,9 +73,13 @@ class Game extends Field{
 
 }
 
-const pawn1 = new Pawn('pb1','white');
+
+const pw1 = new Pawn('pw1', 'white', 'a2'); // создаем 1ю пешку
 
 const game = new Game();
-game.moveTo('white', 'pb1', 4, 5, 3, 9);
+const pw2 = new Pawn('pw2', 'white', 'a3');// создаем еще одну пешку в клетке а3
+game.moveTo(pw1, 'a3'); // пробуем двигать первую пешку из а2 в а3 - Клетка уже занята, сюда ходить нельзя
+
+console.log(pieces);
 
 
